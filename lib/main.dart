5df3d4ae3082c597230a72947a2cefc705ec851c
Abiola Mishael <woroumishael@gmail.com>
2021-07-02 15:04:22 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja Id Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('assets/thumb.png'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.amberAccent,
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  'NAME:',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2,
+                  ),
+                ),
+                Text(
+                  'Nonymous Ray',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Text(
+                  'CURRENT NINJA LEVEL:',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2,
+                  ),
+                ),
+                Text(
+                  '400',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'nonymous@ray.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
